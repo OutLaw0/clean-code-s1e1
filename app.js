da096d44f402e57fc5db2c93659f786343c0cbaa
OutLawO <vaakimov@gmail.com>
2022-05-13 17:01:00 +0300

    refactor: fix html/css formatting according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -33,17 +33,22 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
 
+    listItem.className='list';
+    
+    label.className='label-task task';
+    
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className='input-checkbox';
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input-task task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button delete";
+    deleteButtonImg.className="img";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -82,7 +87,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector('.input-task');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
